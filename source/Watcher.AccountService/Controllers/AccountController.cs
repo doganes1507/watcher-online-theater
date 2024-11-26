113ec98a5ba87123e5d35d5324597a67a5169908
doganes1507 <d.oganes1507@gmail.com>
2024-11-26 21:04:35 +0300

    Initial commit

---
+++ source/Watcher.AccountService/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Watcher.AccountService.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    
+}
---
+++ source/Watcher.IdentityService/Controllers/IdentityController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Watcher.IdentityService.Controllers;
+
+[ApiController]
+public class IdentityController : ControllerBase
+{
+    
+}
