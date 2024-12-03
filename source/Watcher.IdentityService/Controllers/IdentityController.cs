f2436f15596d06a72dce6e4598536dfd0ceee567
doganes1507 <d.oganes1507@gmail.com>
2024-12-03 13:40:22 +0300

    Implement SendEmailCode method in IdentityController
    Add IJwtTokenService interface

--- source/Watcher.IdentityService/Controllers/IdentityController.cs
+++ source/Watcher.IdentityService/Controllers/IdentityController.cs
@@ -1,9 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+using Watcher.IdentityService.Interfaces;
+using Watcher.IdentityService.Models;
 
 namespace Watcher.IdentityService.Controllers;
 
 [ApiController]
 public class IdentityController : ControllerBase
 {
+    private readonly DbContext _dbContext;
+    private readonly IDatabase _redisDatabase;
+    private readonly IEmailService _emailService;
+    private readonly IJwtTokenService _jwtTokenService;
+
+    public IdentityController(DbContext dbContext,
+        IConnectionMultiplexer connectionMultiplexer,
+        IEmailService emailService,
+        IJwtTokenService jwtTokenService)
+    {
+        _dbContext = dbContext;
+        _redisDatabase = connectionMultiplexer.GetDatabase();
+        _emailService = emailService;
+        _jwtTokenService = jwtTokenService;
+    }
+
+    public async Task<IActionResult> SendEmailCode([EmailAddress] string email)
+    {
+        var random = new Random();
+        var confirmationCode = random.Next(100000, 999999);
+        
+        await _redisDatabase.StringSetAsync(email, confirmationCode, TimeSpan.FromMinutes(5));
+        await _emailService.SendConfirmationCode(email, confirmationCode);
+        
+        return Ok("Code successfully sent");
+    }
     
+    
+    public async Task<IActionResult> ConfirmCode([EmailAddress] string email, int code)
+    {
+        return Ok();
+    }
+    
+    public async Task<IActionResult> Login()
+    {
+        return Ok();
+    }
+    
+    public async Task<IActionResult> UpdatePassword()
+    {
+        return Ok();
+    }
 }
---
+++ source/Watcher.IdentityService/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,6 @@
+﻿namespace Watcher.IdentityService.Interfaces;
+
+public interface IJwtTokenService
+{
+    public string GenerateToken(Guid userId, string email);
+}
